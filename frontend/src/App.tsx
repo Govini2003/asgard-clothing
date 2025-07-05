221cc1a489165420473385f678caeba217fb6d41
Govini Rajapakse <160473567+Govini2003@users.noreply.github.com>
2025-07-05 16:51:41 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 // import { Instagram, ShoppingBag, ChevronLeft, ChevronRight } from 'lucide-react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
